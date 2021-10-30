ce64df33b9c9e627db5d422e95720ae10918bb37
Henry Campbell <hacampbell99@gmail.com>
2021-10-30 21:39:43 +1100

    Added Nav component

---
+++ client/src/components/Nav.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="nav">
+        <router-link to="/">HOME</router-link>
+        <router-link to="/about">ABOUT</router-link>
+        <router-link to="/projects">PROJECTS</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+    name: 'Nav',
+    }
+</script>
+
+<style scoped>
+    #nav {
+        text-align: left;
+        padding-left: 2rem;
+        padding-top: 2rem;
+    }
+
+    a {
+        font-weight: bold;
+        margin-right: 3rem;
+        color: white;
+        text-decoration: none;
+
+    }
+
+    a.router-link-active, a:hover {
+        border-bottom: 2px solid #96179b;
+    }
+</style>

1594aaee4bc469e0bc1581d759604a8da8129da2
Henry Campbell <hacampbell99@gmail.com>
2021-11-01 14:01:32 +1100

    Created About component

---
+++ client/src/components/About.vue
@@ -0,0 +1,105 @@
+<template>
+    <div id="about">
+        <!-- <Nav /> -->
+        <h2>About</h2>
+        <h3>Personal Information</h3>
+        <h4>Get to know more about me</h4>
+
+        <div class="row">
+            <!-- Left Column -->
+            <div class="column">
+                <h4>Hello!</h4>
+                <p>
+                    My name is Henry Campbell, and I'm a computer science student at the University of New England majoring in Software Development hoping to become a software engineer. I have particular interest in backend web development, data structures and algorithms, and desktop application development.
+                </p>
+                <p>
+                    I’m currently looking to become part of a dedicated team of engineers responsible for building quality software, so if you’re hiring, please feel free to contact me on LinkedIn. 
+                </p>
+            </div>
+
+            <!-- Right Column -->
+            <div class="column">
+                <h4>Professional Skills</h4>
+                <ul id="skills">
+                    <li>JavaScript</li>
+                    <li>Node + Express</li>
+                    <li>C/C++</li>
+                    <li>Java</li>
+                    <li>C#</li>
+                    <li>Python</li>
+                    <li>SQL</li>
+                    <li>MongoDB</li>
+                    <li>Git</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+    h2 {
+        color: #c83cb9;
+        font-size: 175%;
+        padding-top: 10rem;
+    }
+
+    h3 {
+        font-size: 200%;
+        color: black;
+        padding-top: 1rem;
+    }
+
+    h4 {
+        font-size: 125%;
+        color: black;
+        padding-top: 1rem;
+    }
+
+    p {
+        color: black;
+        margin-top: 1rem;
+        font-size: 110%;
+        line-height: 1.5;
+    }
+
+    ul {
+        margin-top: 1rem;
+    }
+
+    li {
+        color: #333;
+        list-style: none;
+        font-size: 95%;
+        margin: 0.5rem;
+        background-color: #eee;
+        display: inline-block;
+        padding: 1% 4%;
+    }
+
+    #about {
+        background-color: rgb(255, 255, 255);
+        height: 100vh;
+        text-align: center;
+    }
+
+    .row {
+        display: flex;
+        margin-left: 15rem;
+        margin-right: 15rem;
+    }
+
+    .column {
+        flex: 50%;
+        margin: 5rem 1rem 5rem 1rem;
+        text-align: left;
+    }
+
+</style>

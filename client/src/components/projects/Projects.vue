aa78ff4ba010025dd6dc43a6f4e275b30dadfaff
Henry Campbell <hacampbell99@gmail.com>
2021-11-01 19:18:22 +1100

    Updated to use ProjectItem Component

--- client/src/components/projects/Projects.vue
+++ client/src/components/projects/Projects.vue
@@ -3,15 +3,43 @@
         <h2>PROJECTS</h2>
         <h3>Latest Work</h3>
         <h4>View a sample of my work here</h4>
+
+        <div class="row">
+            <div class="column">
+                <ProjectItem
+                    class="project-item"
+                    title="Example Project 1"
+                    description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quo ipsum tenetur, magni numquam cupiditate quisquam harum perspiciatis delectus facilis! Labore sit soluta hic libero accusamus ipsa laboriosam laborum in."
+                    live="https://google.com"
+                    github="https://github.com/"
+                    img="https://web.archive.org/web/20201226002509im_/https://d33wubrfki0l68.cloudfront.net/4343591e0826cbd40384e2841cf9851dec1d2017/9c7bb/images/gainon-mockup.jpg"
+                />
+            </div>
+
+            <div class="column">
+                <ProjectItem
+                    class="project-item"
+                    title="Example Project 2"
+                    description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quo ipsum tenetur, magni numquam cupiditate quisquam harum perspiciatis delectus facilis! Labore sit soluta hic libero accusamus ipsa laboriosam laborum in."
+                    live="https://google.com"
+                    github="https://github.com/"
+                    img="https://web.archive.org/web/20201226002509im_/https://d33wubrfki0l68.cloudfront.net/4343591e0826cbd40384e2841cf9851dec1d2017/9c7bb/images/gainon-mockup.jpg"
+                />
+            </div>
+        </div>
+
     </div>
 </template>
 
 <script>
-export default {
-  name: 'Projects',
-  components: {
-  }
-}
+    import ProjectItem from './ProjectItem.vue'
+
+    export default {
+        name: 'Projects',
+        components: {
+            ProjectItem
+        }
+    }
 </script>
 
 <style scoped>
@@ -36,9 +64,16 @@ export default {
 
     #projects {
         background-color: #e2e2e2;
-        height: 100vh;
+        min-height: 100vh;
         text-align: center;
     }
 
+    .row {
+        display: flex;
+    }
+
+    .column {
+        flex: 50%;
+    }
 
 </style>

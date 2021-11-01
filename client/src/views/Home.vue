cc641a005efbb5c31976b2a1f0de7c88fbd623f5
Henry Campbell <hacampbell99@gmail.com>
2021-11-01 19:22:37 +1100

    Added Projects section to home page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -7,18 +7,24 @@
         <section id="About">
             <About />
         </section>
+
+        <section id="Projects">
+            <Projects />
+        </section>
     </div>
 </template>
 
 <script>
     import Splash from '../components/splash/Splash.vue';
     import About from '../components/about/About.vue';
+    import Projects from '../components/projects/Projects.vue'
 
     export default {
         name: 'Home',
         components: {
             Splash,
-            About
+            About,
+            Projects
         }
     }
 
@@ -26,55 +32,11 @@
 
 <style scoped>
 
-    #content {
+    /* #content {
         height: 100vh;
-    }
+    } */
 
     section {
         height: 100vh;
     }
-    /* h1 {
-        font-size: 400%;
-        text-align: left;
-        padding-left: 35%;
-        z-index: 1;
-    }
-
-    h2 {
-        font-size: 300%;
-        text-align: left;
-        padding-left: 35%;
-        z-index: 1;
-    }
-
-    h3 {
-        color: #c83cb9;
-        padding-top: 20%;
-        font-size: 200%;
-        text-align: left;
-        padding-left: 30%;
-        z-index: 1;
-    }
-
-    #home {
-        background-color: rgb(17, 17, 17);
-        height: 100vh;
-        background-image: url(https://d33wubrfki0l68.cloudfront.net/images/f20315b799c67d762c3c2992f87aa674e6e30d19/hero-bg.jpg);
-        background-repeat: no-repeat;
-        background-size: 100% 100%;
-        position: relative;
-        z-index: 1;
-    }
-
-    .gradient-overlay {
-        width: 100%;
-        height: 100%;
-        background: black;
-        background-image: linear-gradient(to bottom, rgb(20, 20, 20), rgb(0, 0, 0));
-        opacity: 0.9;
-        position: absolute;
-        left: 0;
-        top: 0;
-        z-index: -1;
-    } */
 </style>
